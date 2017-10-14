355e802cb3811fedbd63b431d0b044ffcec38ef0
SwayGatsby <kayla.fowler@hotmail.com>
2017-10-14 17:36:34 +0100

    added who all films method to film class

--- models/film.rb
+++ models/film.rb
@@ -25,5 +25,16 @@ attr_accessor(:price)
     @id = film[0]['id'].to_i
   end
 
+  def self.all()
+    sql = "SELECT * FROM films"
+    values = []
+    films = SqlRunner.run(sql,values)
+    result = films.map{|film| Film.new(film)}
+    return result
+  end
+
+
+
+  
 
 end
